40664e6c975e9951f8cabf3fae4392eb76a6efcb
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-05-22 17:57:57 -0700

    (feat) Configure middleware to use UserRouter and sync database

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,12 +1,21 @@
 ﻿var morgan = require('morgan'); // used for logging incoming request
 var bodyParser = require('body-parser');
 
-module.exports = function(app, express) {
+module.exports = function(app, express, passport) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({
     extended: false
   }));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../dist/'));
-  //require('../code/codeRoutes.js')(app);
+
+  var userRouter = express.Router();
+
+  app.use('/api/users', userRouter);
+  app.use('/auth', userRouter);
+
+  require('../users/passportConfig.js')(passport);
+  require('../users/userRoutes.js')(userRouter);
+
 };
+
