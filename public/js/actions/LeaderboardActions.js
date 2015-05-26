11dc064715d18cf61456dbabd8cf525743ab4a4b
Lauren <lhspiegel@gmail.com>
2015-05-26 15:33:14 -0700

    (refactor) Refactor leaderboard component to flux structure.

--- public/js/actions/LeaderboardActions.js
+++ public/js/actions/LeaderboardActions.js
@@ -2,43 +2,114 @@
 
 var AppDispatcher = require('../dispatcher/AppDispatcher');
 var AppConstants = require('../constants/AppConstants');
+var config  = require('../../env/config.js');
 
-var LeaderBoardActions = {
+var instagramKey = config.INSTAGRAM_CLIENT_ID;
+var instagramTokenLabel = 'access_token=';
+
+var LeaderboardActions = {
 
 
-  //Revise to run functions to get User's key, then get leaders, then get pics.  
+getPics: function(topScorerInstagramIDs){
 
-  getUserKey: function(email, password){
-    var user = { email: email, password: password};
+    var pictures = [];
+
+    for(var i=0; i< topScorerInstagramIDs.length; i++){
+      var topUserInstagramID = topScorerInstagramIDs[i];
+      var url = 'https://api.instagram.com/v1/users/' + topUserInstagramID + '/media/recent/?' + instagramTokenLabel + instagramKey;
+
+      $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        type: 'GET',
+        success: function(result){
+
+          if(!result.data){
+              console.log("Error message from Instagram:", result.meta.error_message);
+              return;
+          } else {
+
+            pictures.push({ 
+                    user: result.data[0].user.username,
+                    id: result.data[0].id, 
+                    url: result.data[0].link, 
+                    src: result.data[0].images.low_resolution.url, 
+                    title: result.data[0].caption ? result.data[0].caption.text : ''
+                });
+
+            //Send the updated picture array through the dispatcher to the LeaderboardStore
+
+            AppDispatcher.handleServerAction({
+              actionType: AppConstants.GET_PICS,
+              data: pictures
+            })
+
+          }
+
+        },
+        error: function(xhr, status, error){
+          console.error(xhr, status, error)
+        }
+      })
+
+      }
+
+    },
+
+
+  getTopScorers: function(){
+
+    var self = this;
+
     $.ajax({
-      url: '/api/users/login',
+      url: '/api/users/leaders',
       dataType: 'json',
-      type: 'POST',
-      data: user,
+      type: 'GET',
       success: function(data){
-        AppDispatcher.handleViewAction({
-          actionType: AppConstants.LOGIN_USER,
-          data: data
-        })
+          
+        //Once get leaders, call to instagram to get photos.  Data is the array of Instagram user IDs.
+        self.getPics(data);
+        
       },
       error: function(xhr, status, error){
         console.error(xhr, status, error)
-      }.bind(this) //NOTE: we may need a .bind(this) here-ish
+      }.bind(this) 
     })
+
   },
 
-  getLeaders: function(){
-    AppDispatcher.handleViewAction({
-      actionType: AppConstants.LOGOUT_USER
+  getAccessToken: function(username){
+    var self = this;
+
+    $.ajax({
+      url: '/api/users/instagramKey',
+      data: { name: username },
+      dataType: 'json',
+      type: 'GET',
+      success: function(data){
+
+        //If received the current user's instagram token back from server, change the 
+        //instagram key to the user's token and change the lable in the URL to access_token
+
+        if(data){
+          instagramKey = data;
+          instagramTokenLabel = 'access_token='
+        }
+
+        //Call to server to get leaders
+        self.getTopScorers();
+
+      },
+      error: function(xhr, status, error){
+        console.error(xhr, status, error)
+        
+        self.getTopScorers();
+
+      }.bind(this) 
     })
-  },
-
-
-  getPics: function(){
-
-
   }
 
-}
+};
 
 module.exports = LeaderboardActions;
+
--- public/js/components/Leaderboard.js
+++ public/js/components/Leaderboard.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var React = require('react/addons');
-var config  = require('../../env/config.js');
-var instaKey = config.INSTAGRAM_CLIENT_ID;
-
-//user's key.  if they don't have key, we need to use our client_id -- in config file
-//and need to change access_token in url to client_id
+var LeaderboardActions = require('../actions/LeaderboardActions');
+var LeaderboardStore = require('../stores/LeaderboardStore');
 
 
 var Leaderboard = React.createClass({
@@ -24,15 +21,21 @@ var Leaderboard = React.createClass({
       
       return { 
         pictures: [],
-        username: '',
-        instagramKey: instaKey,
-        instagramTokenLabel: 'client_id=',
+        username: ''
       };
   },
 
+
+  _onChange : function(){
+    this.setState ({
+      pictures: LeaderboardStore.getPictures()
+    })
+  },
+
+
   componentDidMount: function(){
 
-      var self = this;
+    LeaderboardStore.addChangeListener(this._onChange);
 
       var username = this.getParameterByName("name");
       this.setState({
@@ -40,99 +43,25 @@ var Leaderboard = React.createClass({
       });
 
       
-      var getPics = function(topScorerInstagramIDs){
-
-        var pictures = [];
-
-        for(var i=0; i< topScorerInstagramIDs.length; i++){
-          var userInstagramID = topScorerInstagramIDs[i];
-          var url = 'https://api.instagram.com/v1/users/' + userInstagramID + '/media/recent/?' + self.state.instagramTokenLabel + self.state.instagramKey;
-
-          $.ajax({
-            url: url,
-            dataType: 'jsonp',
-            type: 'GET',
-            success: function(result){
-
-              if(!result.data){
-                  console.log("Error message from Instagram:", result.meta.error_message);
-                  return;
-              } else {
-              console.log(result.data);
-
-              pictures.push({ 
-                      user: result.data[0].user.username,
-                      id: result.data[0].id, 
-                      url: result.data[0].link, 
-                      src: result.data[0].images.low_resolution.url, 
-                      title: result.data[0].caption ? result.data[0].caption.text : ''
-                  });
-
-              // Update the component's state. This will trigger a render.
-
-              self.setState({ pictures: pictures });
-            }
-
-            },
-            error: function(xhr, status, error){
-              console.error(xhr, status, error)
-            }
-          })
-
-          }
-
-        };
-
-
-        var getTopScorers = function(){
-          $.ajax({
-            url: '/api/users/leaders',
-            dataType: 'json',
-            type: 'GET',
-            success: function(data){
-                
-              //Once get leaders, call to instagram to get photos.  Data is the array of Instagram user IDs.
-              getPics(data);
-              
-            },
-            error: function(xhr, status, error){
-              console.error(xhr, status, error)
-            }.bind(this) 
-          })
-
-        };
-
-
 
       //If have a username in params, send an AJAX request to the server to get the signed in user's instagram access toekn.
 
       if(username){
-        $.ajax({
-          url: '/api/users/instagramKey',
-          data: { name: username },
-          dataType: 'json',
-          type: 'GET',
-          success: function(data){
-            //If received the user's access token back, change the instagram key to the 
-            //user's token and chnage the label to be used in the URS to access_token
-            self.setState({instgramKey: data, instgramTokenLabel: 'access_token='})
+        LeaderboardActions.getAccessToken(username);
 
-              //Call to server to get leaders
-              getTopScorers();
-
-          },
-          error: function(xhr, status, error){
-            console.error(xhr, status, error)
-            getTopScorers();
-          }.bind(this) 
-        })
       } else {
         //If don't have a user who signed in with instagram, just go ahead and get top scorers
-        getTopScorers();
+        LeaderboardActions.getTopScorers();
 
       }
 
   },
+
+
+  componentWillUnmount: function() {
+    LeaderboardStore.removeChangeListener(this._onChange);
+  },
+
 
   showUser: function(user){
     //Create function when mouseover image, show username.
