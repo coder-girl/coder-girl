7dc4f7c7c351d73c32db3245692d270f75c3d4e6
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-28 14:37:10 -0700

    (feat) Add welcome component and empty challengeActions.js

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -2,7 +2,7 @@
 * @Author: Mark Bennett
 * @Date:   2015-05-27 19:54:19
 * @Last Modified by:   Mark Bennett
-* @Last Modified time: 2015-05-27 20:04:46
+* @Last Modified time: 2015-05-28 14:36:37
 */
 
 'use strict';
@@ -14,7 +14,10 @@ var Welcome = React.createClass({
   render: function() {
     var user = AuthStore.getUser();
     return (
-
+      <div className="welcome-wrapper"> 
+        <h1>WELCOME VIEW COMPONENT</h1> 
+        <button onClick="someFuncInChallengeActions.js">Continue coding</button>
+      </div>
     )
   }
 });
