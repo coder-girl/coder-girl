dfe9de8774300ad7ea0d8ae6f3dc86921a92358d
Mark Bennett <mwbennett@wesleyan.edu>
2015-05-29 19:14:48 -0700

    (fix) Add post_install script for deployment

---
+++ post_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# @Author: Mark Bennett
+# @Date:   2015-05-29 19:14:00
+# @Last Modified by:   Mark Bennett
+# @Last Modified time: 2015-05-29 19:14:32
+
+#install client side dependencies
+./node_modules/bower/bin/bower install
