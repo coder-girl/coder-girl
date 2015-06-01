73497c7de0f246686d2037558ea59ebd1831d1e4
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-01 11:54:00 -0700

    (feat) Add route for adding code challenges

--- server/challenges/codeChallengeRoutes.js
+++ server/challenges/codeChallengeRoutes.js
@@ -2,7 +2,7 @@
 * @Author: nimi
 * @Date:   2015-05-25 11:12:28
 * @Last Modified by:   nimi
-* @Last Modified time: 2015-05-28 19:57:42
+* @Last Modified time: 2015-06-01 11:53:41
 */
 
 'use strict';
@@ -11,4 +11,5 @@ var codeChallenge = require('./codeChallengeController.js');
 
 module.exports = function(app){
   app.get('/getChallenge/:testCode', codeChallenge.getChallenge);
+  app.post('/addChallenge', codeChallenge.addChallenge);
 }
