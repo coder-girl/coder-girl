75db38858b1f1a3e8ba677d75e87aa786c53290c
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-01 12:33:36 -0700

    (feat) Add function to create challenge in the database if it doesn't already exist

--- server/challenges/codeChallengeController.js
+++ server/challenges/codeChallengeController.js
@@ -2,7 +2,7 @@
 * @Author: nimi
 * @Date:   2015-05-25 11:12:43
 * @Last Modified by:   nimi
-* @Last Modified time: 2015-05-28 20:13:12
+* @Last Modified time: 2015-06-01 12:31:49
 */
 
 'use strict';
@@ -17,5 +17,45 @@ module.exports = {
       .then(function(challenge){
         res.json(challenge)
       })
+  },
+
+  addChallenge: function(req, res, next){
+    var challenge = req.body;
+
+    if(!challenge.hasOwnProperty('title')){
+
+      next(new Error('Challenges must have a title'));
+
+    } else if (!challenge.hasOwnProperty('content')){
+
+      next(new Error('Challenges must have content'));
+
+    } else if (!challenge.hasOwnProperty('instructions')){
+
+      next(new Error('Challenges must have instructions'));
+
+    } else if(!challenge.hasOwnProperty('testCode')){
+
+      next(new Error('Challenges must have a testCode'));
+
+    } else if(!challenge.hasOwnProperty('pointValue')) {
+
+      next(new Error('Challenges must have point value'));
+    }
+
+    Challenge.findOrCreate({ 
+      where : {
+        title: req.body.title,
+        content: req.body.content,
+        instructions: req.body.instructions,
+        testCode: req.body.testCode,
+        pointValue: req.body.pointValue
+    }})
+    .then(function(challenge){
+      res.sendStatus(200)
+    })
+    .error(function(error){
+      res.json('error?')
+    })
   }
 }
