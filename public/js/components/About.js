8998e6ac2ef2f144953d1a3028fe4536a78fea61
David Trinh <dhtrinh02@gmail.com>
2015-06-05 11:37:14 -0700

    (feat) Add Coder-Girl's Theme Song

--- public/js/components/About.js
+++ public/js/components/About.js
@@ -1,11 +1,11 @@
 var React = require('react');
 
 var About = React.createClass({
-  
   render: function() {
     return (
       <div>
         <h1>Best site ever in the whole wide world!</h1>
+        <audio className="hidden" src="/asset/SkaterboyMeetsTechGirl_prod_kybate.mp3" controls autoPlay loop></audio> 
       </div>
     );
   }
