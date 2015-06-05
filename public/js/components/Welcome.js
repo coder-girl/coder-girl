d5abb48151c90a68af72ee8ef9a9c7713d6d94c6
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-05 15:19:07 -0700

    (feat) Create basic canvas element with challenge road

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -1,14 +1,15 @@
 /* 
 * @Author: Mark Bennett
 * @Date:   2015-05-27 19:54:19
-* @Last Modified by:   Mark Bennett
-* @Last Modified time: 2015-05-30 16:50:51
+* @Last Modified by:   nimi
+* @Last Modified time: 2015-06-05 12:35:41
 */
 
 'use strict';
 
 var React = require('react');
 var AuthStore = require('../stores/AuthStore');
+var paper = require('../../../dist/bower_components/paper/dist/paper-full')
 
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
@@ -29,6 +30,50 @@ var Welcome = React.createClass({
   },
 
   componentDidMount: function() {
+    var canvas = document.getElementById('welcomeBoard');
+    var width = canvas.offsetWidth;
+
+    // points will be equally distributed across x-axis
+    var x = width/5;
+
+    // create the points for the path
+    var pointsCreator = function(distancebetweenPoints, numberofPoints){
+      var results = []
+      var direction = 1;
+      var x = 20;
+      var y = canvas.offsetHeight - 50;
+      for(var i = 0; i <= numberofPoints; i++){
+        direction*= -1; 
+        var coordinates = {}
+        coordinates.x = x;
+        coordinates.y = y + (5*direction);
+        results.push(coordinates);
+        x+= distancebetweenPoints
+        if(x >= width){
+          x = width - 20;
+        }
+      }
+
+      return results;
+    }
+     var points = pointsCreator(x, 5);
+    paper.setup(canvas);
+
+    var levelLine = new paper.Path();
+    levelLine.strokeColor = 'black';
+    levelLine.strokeWidth = 10;
+
+    points.forEach(function(point){
+      levelLine.add(new paper.Point(point.x, point.y))
+      var circle = new paper.Path.Circle(new paper.Point(point.x, point.y), 10)
+      circle.fillColor = 'white';
+      circle.strokeColor = 'black'
+    })
+
+    levelLine.closed = false;
+    levelLine.smooth();
+
+    paper.view.draw()
     this.setState ({
       user: AuthStore.getUser()
     })
@@ -47,12 +92,10 @@ var Welcome = React.createClass({
 
     return (
       <div className="welcome-container"> 
-        <h1 className="welcome-title">
+        <div className="welcome-title">
           {greeting}, <span className="welcome-username">{this.state.user.username}</span>
-        </h1> 
-        <div className="welcome-user-level">Challenge: {this.state.user.challengeNumber - 1}</div>
-        <div className="welcome-user-score">Score: {this.state.user.score} points</div>
-        <Link to="challenge" className="link-to-code">Continue coding</Link>
+        </div> 
+        <canvas id="welcomeBoard"> </canvas>
       </div>
     );
   }
