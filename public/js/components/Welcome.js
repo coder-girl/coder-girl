346ac7565604c9bc49eae920c764c4a4179b76a6
David Trinh <dhtrinh02@gmail.com>
2015-06-06 15:04:56 -0700

    (feat) Add various div and css styling into Welcome.js and style.css for Weclome
    Adds
      -Sun
      -Clouds

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -1,15 +1,15 @@
 /* 
-* @Author: Mark Bennett
-* @Date:   2015-05-27 19:54:19
-* @Last Modified by:   nimi
-* @Last Modified time: 2015-06-05 12:35:41
-*/
+ * @Author: Mark Bennett
+ * @Date:   2015-05-27 19:54:19
+ * @Last Modified by:   nimi
+ * @Last Modified time: 2015-06-05 12:35:41
+ */
 
 'use strict';
 
 var React = require('react');
 var AuthStore = require('../stores/AuthStore');
-var paper = require('../../../dist/bower_components/paper/dist/paper-full')
+var paper = require('../../../dist/bower_components/paper/dist/paper-full');
 
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
@@ -20,63 +20,62 @@ var Welcome = React.createClass({
   getInitialState: function() {
     return {
       user: {}
-    }
+    };
   },
 
-  _onChange : function(){
-    this.setState ({
+  _onChange: function() {
+    this.setState({
       user: AuthStore.getUser()
-    })
+    });
   },
 
   componentDidMount: function() {
     var canvas = document.getElementById('welcomeBoard');
     var width = canvas.offsetWidth;
-
     // points will be equally distributed across x-axis
-    var x = width/5;
+    var x = width / 5;
 
     // create the points for the path
-    var pointsCreator = function(distancebetweenPoints, numberofPoints){
-      var results = []
+    var pointsCreator = function(distancebetweenPoints, numberofPoints) {
+      var results = [];
       var direction = 1;
       var x = 20;
       var y = canvas.offsetHeight - 50;
-      for(var i = 0; i <= numberofPoints; i++){
-        direction*= -1; 
-        var coordinates = {}
+      for (var i = 0; i <= numberofPoints; i++) {
+        direction *= -1;
+        var coordinates = {};
         coordinates.x = x;
-        coordinates.y = y + (5*direction);
+        coordinates.y = y + (5 * direction);
         results.push(coordinates);
-        x+= distancebetweenPoints
-        if(x >= width){
+        x += distancebetweenPoints;
+        if (x >= width) {
           x = width - 20;
         }
       }
 
       return results;
-    }
-     var points = pointsCreator(x, 5);
+    };
+    var points = pointsCreator(x, 5);
     paper.setup(canvas);
 
     var levelLine = new paper.Path();
     levelLine.strokeColor = 'black';
     levelLine.strokeWidth = 10;
 
-    points.forEach(function(point){
-      levelLine.add(new paper.Point(point.x, point.y))
-      var circle = new paper.Path.Circle(new paper.Point(point.x, point.y), 10)
+    points.forEach(function(point) {
+      levelLine.add(new paper.Point(point.x, point.y));
+      var circle = new paper.Path.Circle(new paper.Point(point.x, point.y), 10);
       circle.fillColor = 'white';
-      circle.strokeColor = 'black'
-    })
+      circle.strokeColor = 'black';
+    });
 
     levelLine.closed = false;
     levelLine.smooth();
 
-    paper.view.draw()
-    this.setState ({
+    paper.view.draw();
+    this.setState({
       user: AuthStore.getUser()
-    })
+    });
     AuthStore.addChangeListener(this._onChange);
   },
 
@@ -85,20 +84,37 @@ var Welcome = React.createClass({
   },
 
   render: function() {
-    var greeting = "Welcome";
+    var greeting = 'Welcome';
     if (this.state.user.challengeNumber > 1) {
-      greeting += " back";
+      greeting += ' back';
     }
 
     return (
-      <div className="welcome-container"> 
+      <div className="welcome-container">
         <div className="welcome-title">
-          {greeting}, <span className="welcome-username">{this.state.user.username}</span>
-        </div> 
-        <canvas id="welcomeBoard"> </canvas>
+          {greeting},
+          <span className="welcome-username">{this.state.user.username}</span>
+        </div>
+        <canvas id="welcomeBoard"></canvas>
+        <div className="sun">
+          <div className="sun-face">
+            <div className="sun-hlight"></div>
+            <div className="sun-leye"></div>
+            <div className="sun-reye"></div>
+            <div className="sun-lred"></div>
+            <div className="sun-rred"></div>
+            <div className="sun-nose"></div>
+          </div>
+          <div className="sun-anime">
+            <div className="sun-ball"></div>
+          </div>
+        </div>
+        <div id="cloud">
+          <div className="clouds cloud-1"></div>
+        </div>
       </div>
     );
   }
 });
 
-module.exports = Welcome; 
+module.exports = Welcome;
