9f4b85b3697aa6bb129bfd93523e73b14c1cf242
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-05 20:36:03 -0700

    (feat) Add transitions to fill in circles of completed challenges

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -1,17 +1,18 @@
 /* 
-<<<<<<< HEAD
  * @Author: Mark Bennett
  * @Date:   2015-05-27 19:54:19
  * @Last Modified by:   nimi
- * @Last Modified time: 2015-06-06 16:58:29
+ * @Last Modified time: 2015-06-06 17:00:26
  */
 
 'use strict';
 
 var React = require('react');
 var AuthStore = require('../stores/AuthStore');
-var paper = require('../../../dist/bower_components/paper/dist/paper-full');
+var paper = require('../../../dist/bower_components/paper/dist/paper-full')
+var animatePaper = require('../services/animatePaper.js')
 
+console.log('animate', animatePaper)
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
 var Link = Router.Link;
@@ -27,12 +28,16 @@ var Welcome = React.createClass({
   _onChange: function() {
     this.setState({
       user: AuthStore.getUser()
-    });
+    })
+    
   },
 
   componentDidMount: function() {
+    var user = AuthStore.getUser()
+    console.log(user)
     var canvas = document.getElementById('welcomeBoard');
     var width = canvas.offsetWidth;
+    var count = 1;
     // points will be equally distributed across x-axis
     var distance = width/5;
 
@@ -42,6 +47,7 @@ var Welcome = React.createClass({
       var direction = 1;
       var x = distanceBetweenPoints/2;
       var y = canvas.offsetHeight - 50;
+      
       for(var i = 0; i < numberofPoints; i++){
         direction*= -1; 
         var coordinates = {}
@@ -56,7 +62,8 @@ var Welcome = React.createClass({
 
       return results;
     }
-     var points = pointsCreator(distance, 5);
+
+    var points = pointsCreator(distance, 5);
     paper.setup(canvas);
 
     var levelLine = new paper.Path();
@@ -64,20 +71,61 @@ var Welcome = React.createClass({
     levelLine.strokeWidth = 10;
     levelLine.add(new paper.Point(0, (points[0].y-20)))
 
+    // add a path point and a circle at every point in the array, also add the level number (hardcoded in for now)
     points.forEach(function(point){
       levelLine.add(new paper.Point(point.x, point.y))
-      var circle = new paper.Path.Circle(new paper.Point(point.x, point.y), 20)
-      circle.fillColor = 'white';
-      circle.strokeColor = 'black';
-    });
+
+      point.circle = new paper.Path.Circle(new paper.Point(point.x, point.y), 20);
+      point.circle.fillColor = 'white';
+      point.circle.strokeColor = 'black'
+      point.level= new paper.PointText({
+        point: [point.x-5, point.y+5],
+        content: count,
+        fillColor: 'black',
+        fontFamily: 'Courier New',
+        fontWeight: 'bold',
+        fontSize: 20
+      });
+      count++
+    })
 
     levelLine.add(new paper.Point((width-1), (points[points.length-1].y - 20)))
 
     levelLine.closed = false;
     levelLine.smooth();
 
-    paper.view.draw();
-    this.setState({
+    //animate to the current challenge
+    //
+    //loop through points until you get to the current challenge
+    //fill in each circle with blue
+    //do a cool shakey thing
+    //move on
+    
+    var counter = -1;
+    (function next() {
+    if (counter >= 2) return;
+
+    setTimeout(function() {
+      var overlayCircle =  new paper.Path.Circle(new paper.Point(points[counter].x, points[counter].y), 20)
+      overlayCircle.opacity = 0;
+      overlayCircle.fillColor = '#5BC0BA'
+      overlayCircle.animate({
+      properties: {
+        opacity:1
+      },
+      settings: {
+        duration: 2000,
+        easing: "swing"
+      }
+    });
+        next();
+    }, 1000);
+    counter++
+  })();
+    
+
+    paper.view.draw()
+    this.setState ({
       user: AuthStore.getUser()
     });
     AuthStore.addChangeListener(this._onChange);
