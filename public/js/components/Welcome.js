16d2418c3726f205a90080c604a1b0412b3f6bd6
Nimi Dharithreesan <nimi.dharithreesan@gmail.com>
2015-06-06 16:28:10 -0700

    (feat) Add User Bubble to canvas that moves along levelLine

--- public/js/components/Welcome.js
+++ public/js/components/Welcome.js
@@ -1,9 +1,10 @@
 /* 
 <<<<<<< HEAD
+<<<<<<< HEAD
  * @Author: Mark Bennett
  * @Date:   2015-05-27 19:54:19
  * @Last Modified by:   nimi
- * @Last Modified time: 2015-06-06 17:00:59
+ * @Last Modified time: 2015-06-06 17:01:30
  */
 
 'use strict';
@@ -34,10 +35,11 @@ var Welcome = React.createClass({
   },
 
   componentDidMount: function() {
+    paper.install(window);
     var user = AuthStore.getUser()
     console.log(user)
     var canvas = document.getElementById('welcomeBoard');
-    var project = paper.setup(canvas);
+    paper.setup(canvas);
     var width = canvas.offsetWidth;
     var count = 1;
     // points will be equally distributed across x-axis
@@ -63,7 +65,15 @@ var Welcome = React.createClass({
       }
 
       return results;
+    };
+
+    // this function will take in path data and create the objects for the screen from svg pathData
+    var pathCreator = function(pathData){
+      var path = new paper.Path(pathData);
+      return path;
     }
+
+    
 
     var points = pointsCreator(distance, 5);
 
@@ -71,6 +81,12 @@ var Welcome = React.createClass({
     levelLine.strokeColor = 'black';
     levelLine.strokeWidth = 10;
     levelLine.add(new paper.Point(0, (points[0].y-20)))
+    var userBubble = pathCreator("m 245.41842,595.31803 c 0,0 9.0884,-18.5276 -52.50162,-81.84478 -61.93879,-63.67572 -59.96515,-94.64594 -59.73994,-123.27872 0.29053,-36.93671 44.82182,-118.16526 106.05953,-119.90096 54.12684,-1.53415 115.70215,75.80676 115.56646,119.60555 -3.65588,90.09688 -68.26674,132.12215 -109.38443,205.41891 z")
+    userBubble.scaling = 0.22;
+    userBubble.fillColor = '#8E2BC8';
+    userBubble.strokeColor = '#5BC0BA';
+    userBubble.strokeWidth = 2;
+    userBubble.position = new paper.Point(0- (distance/2), (points[0].y-70));
 
     // add a path point and a circle at every point in the array, also add the level number (hardcoded in for now)
     points.forEach(function(point){
@@ -94,37 +110,57 @@ var Welcome = React.createClass({
 
     levelLine.closed = false;
     levelLine.smooth();
-
-    //animate to the current challenge
-    //
-    //loop through points until you get to the current challenge
-    //fill in each circle with blue
-    //do a cool shakey thing
-    //move on
     
     var counter = -1;
     (function next() {
-    if (counter >= 2) return;
+      if(counter >= 2){
+          return
+      } else {
+        setTimeout(function() {
+          var overlayCircle =  new paper.Path.Circle(new paper.Point(points[counter].x, points[counter].y), 20)
+          overlayCircle.opacity = 0;
+          overlayCircle.fillColor = '#5BC0BA'
+          overlayCircle.animate({
+            properties: {
+              opacity:1
+            },
+            settings: {
+              duration: 1500,
+              easing: "swing"
+            }
+          });
+          userBubble.animate({
+            properties :{
+              position: {
+                x: "+" + distance, 
+                y: points[counter].y- 70
+              }
+            },
+            settings: {
+            duration: 1500,
+            easing: "swing"
+            }
+          })
+            next();
+        }, 1500);
+        counter++
+      }   
+    })();
 
-    setTimeout(function() {
-      var overlayCircle =  new paper.Path.Circle(new paper.Point(points[counter].x, points[counter].y), 20)
-      overlayCircle.opacity = 0;
-      overlayCircle.fillColor = '#5BC0BA'
-      overlayCircle.animate({
-      properties: {
-        opacity:1
-      },
-      settings: {
-        duration: 2000,
-        easing: "swing"
-      }
-    });
-        next();
-    }, 1000);
-    counter++
-  })();
-    
-
+    setTimeout(function(){
+      userBubble.animate({
+            properties :{
+              position: {
+                x: "+" + distance, 
+                y: points[counter+1].y- 70
+              }
+            },
+            settings: {
+            duration: 1500,
+            easing: "swing"
+            }
+          })
+    }, 6100)
     paper.view.draw()
     this.setState ({
       user: AuthStore.getUser()
