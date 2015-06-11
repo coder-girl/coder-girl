55fb2e3165aeff17b0d5f7fdd4dc5f5278170800
Lauren <lhspiegel@gmail.com>
2015-06-11 15:10:09 -0700

    (feat) Revise challenge 3 (puppies v. kitten) test.

--- public/js/tests/3.js
+++ public/js/tests/3.js
@@ -1,12 +1,12 @@
 ﻿//Variables: Puppies v. Kittens
 
 var test = function(userCode){
- eval(userCode);
+ var userVariable eval(userCode);
  var result = {
    pass: true,
    message: "You got it!"
  }
- if(puppiesVkittens() !== "puppy" && puppiesVkittens() !== "kittens"){
+ if(userVariable !== "puppy" && userVariable !== "kittens"){
    result.pass = false;
    result.message = "Uh oh, it looks like DuchessFlufferton has not been assigned to either 'puppy' or 'kitten'.  Try again.";
    return result;
