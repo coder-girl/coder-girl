e65216396b811bf31300511f30a8babc5a1a8c0d
David Trinh <dhtrinh02@gmail.com>
2015-06-11 19:06:50 -0700

    (feat) Add challenge test 11 to 15

---
+++ public/js/tests/14.js
@@ -0,0 +1,13 @@
+﻿var test = function(userCode) {
+  var codeForloop = eval(userCode);
+  var result = {
+    pass: true,
+    message: 'You got it, time to head on over to the mall!'
+  };
+  if (codeForloop !== 'for(var i = 0; i < 30; i++) {\'I will not text during class.\'}') {
+    result.pass = false;
+    result.message = 'Not quite.  Keep trying!';
+    return result;
+  }
+  return result;
+};
